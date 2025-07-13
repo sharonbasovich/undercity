5cb424de13e7b8d4a5ce3bda67e5d3d099d28aa0
udu3324 <builder7177@gmail.com>
2025-07-12 20:09:11 -0700

    flask

--- pi-clientside-flask/__init__.py
+++ pi-clientside-flask/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask # type: ignore
+from flask import render_template # type: ignore
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
 
 if __name__ == "__main__":
     app.run()
