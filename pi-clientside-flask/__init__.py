cf772cce493d590036795f91d893e16301fc8814
udu3324 <builder7177@gmail.com>
2025-07-13 00:48:54 -0700

    update

--- pi-clientside-flask/__init__.py
+++ pi-clientside-flask/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask # type: ignore
-from flask import render_template # type: ignore
+from flask import Flask, render_template, redirect, url_for, request # type: ignore
+
+import json
 
 app = Flask(__name__)
 
@@ -7,13 +8,27 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
+@app.route('/redirect-to-catalog', methods=['POST'])
+def redirect_to_catalog():
+    return redirect(url_for('catalog'))
 
-@app.route('/about')
-def about():
-    return 'The about page'
+# loading items from json for catalog
+def load_items():
+    with open('items.json') as f:
+        return json.load(f)
+
+@app.route('/catalog/')
+def catalog():
+    items = load_items()
+    return render_template('catalog.html', items = items)
+
+@app.route('/code/')
+def code():
+    return render_template('code.html')
+
+@app.route('/result/')
+def result():
+    return render_template('result.html')
 
 if __name__ == "__main__":
     app.run()
